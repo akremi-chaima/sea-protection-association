69a8015393b5109c12d6712f840ff43674f03369
Chaima <chaima.akremi.1997@gmail.com>
2023-10-06 14:44:19 +0200

    [EVOL] - Add About us section

---
+++ src/app/about-us/about-us.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-about-us',
+  templateUrl: './about-us.component.html',
+  styleUrls: ['./about-us.component.css']
+})
+export class AboutUsComponent {
+  @Input()
+  hideBanner: boolean | undefined
+}
