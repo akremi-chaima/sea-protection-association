cc04ed81f8b87c6c3f8ba196c4c4e267ab9202a0
Chaima <chaima.akremi.1997@gmail.com>
2024-03-03 17:01:50 +0100

    [EVOL] - Delete news

--- src/app/administration/delete-news/delete-news.component.ts
+++ src/app/administration/delete-news/delete-news.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { NewsService } from '../../services/news.service';
 
 @Component({
   selector: 'app-delete-news',
   templateUrl: './delete-news.component.html',
   styleUrls: ['./delete-news.component.css']
 })
-export class DeleteNewsComponent {
+export class DeleteNewsComponent implements OnInit {
 
+  constructor(
+    private newsService: NewsService,
+    private route: ActivatedRoute,
+    private router: Router,
+  ) {
+  }
+
+  ngOnInit() {
+    if (!this.route.snapshot.paramMap.get('id')) {
+      this.cancel();
+    }
+  }
+
+  delete() {
+    this.newsService.delete(parseInt(this.route.snapshot.paramMap.get('id'), 10)).subscribe(
+      response => {
+        this.cancel();
+      }, error => {
+        if (error.status === 401) {
+          // delete token from local storage and redirect to login page
+          this.router.navigate(['administration/login']);
+          localStorage.removeItem('token');
+        }
+      }
+    );
+  }
+
+  cancel() {
+    this.router.navigate(['administration/news']);
+  }
 }
+
